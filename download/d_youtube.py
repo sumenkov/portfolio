21caa1805a63640a1cfc3aba8a3de97e4937e11c
sdv <sumenkov.dv@yandex.ru>
2022-01-07 20:21:02 +0700

    загрузка видео

--- download/d_youtube.py
+++ download/d_youtube.py
@@ -1,9 +1,23 @@
+from pytube import YouTube
+from pytube.exceptions import VideoUnavailable
+
 from download.utils import check_out_url
 
 
 def run_prm():
-    url, filename, content = check_out_url.ch_url()
-    pass
+    url, _, _ = check_out_url.ch_url()
+    # url = https://www.youtube.com/watch?v=NyN23EVyXYA
+    try:
+        yt = YouTube(url)
+    except VideoUnavailable:
+        print(f'Видео {url} недоступно, пропускаю.')
+    else:
+        print(f'Загрузка видео : {url}')
+        try:
+            yt.streams.get_highest_resolution().download()
+        except VideoUnavailable:
+            print(f'Это видео {url} больше не доступно, пропускаю.')
+    print("Закончили.")
 
 
 if __name__ == '__main__':
